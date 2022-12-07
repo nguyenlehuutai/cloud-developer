d72c6c0cf6ff5d869c180a1e8d8aefe80152c454
TaiNguyen <nguyenlehuutai@gmail.com>
2022-12-07 14:44:07 +0700

    TODO1 IMPLEMENT A RESTFUL ENDPOINT

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -14,23 +14,36 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.use(bodyParser.json());
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
   
+  // GET /filteredimage?image_url={{URL}}
+  app.get( "/filteredimage", async ( req, res ) => {
+    
+    const image_url: string = req.query.image_url;
+    
+    // 1. validate the image_url query
+    if (image_url == null || image_url.length === 0) {
+      res.json({message : 'image_url is required.'});
+    }
+    
+    let file: string;
+    // 2. call filterImageFromURL(image_url) to filter the image
+    await filterImageFromURL(image_url).then(
+      result => {
+          const files = [result];
+          // 3. send the resulting file in the response
+          res.sendFile(result, () => {
+            // 4. deletes any files on the server on finish of the response
+            deleteLocalFiles(files);
+          });
+        file = result;
+      },
+      error => {
+        res.json({message : error});
+      }
+    );
+  });
+  //! END @TODO1
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
