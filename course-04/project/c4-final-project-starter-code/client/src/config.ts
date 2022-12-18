1ad71da1183f4609aa33aa55d1927fd2655ddfc4
TaiNguyen <nguyenlehuutai@gmail.com>
2022-12-18 09:16:36 +0700

    client config serverless app

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'q0ksa5jp3k'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'tainguyen.auth0.com',            // Auth0 domain
+  clientId: 'Syr0TMxkWYyIIXF7dhXYD3g7DrE9a3N4',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
